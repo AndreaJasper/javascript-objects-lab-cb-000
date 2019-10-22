875b7d09fa7670611ed7060ba43a4fe82deccb46
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-10-22 17:41:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object
 }
+
+const deleteFromObjectByKey = (object, key) => {
+  let res = Object.assign({}, object)
+  delete res[key]
+  return res
+}
